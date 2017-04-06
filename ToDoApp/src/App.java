24541cef3054abe6370c9e8ae0b4c1cfe23a05a8
Daniel Szalay <sza.dan.uj@gmail.com>
2017-04-06 15:49:52 +0200

    separate functions from main, create separate functions for list commands

--- ToDoApp/src/App.java
+++ ToDoApp/src/App.java
@@ -1,34 +1,8 @@
-import java.util.Arrays;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
 public class App {
 
   public static void main(String[] args) {
 
-    if (args.length == 0) {
-      System.out.println(
-          "\n" +
-              "*****************************\n" +
-              "* Java Todo application     *\n" +
-              "* =====================     *\n" +
-              "*                           *\n" +
-              "* Command line arguments:   *\n" +
-              "*  -l   Lists all the tasks *\n" +
-              "*  -a   Adds a new task     *\n" +
-              "*  -r   Removes a task      *\n" +
-              "*  -c   Completes a task    *\n" +
-              "*****************************\n" +
-              "\n"
-      );
-    } else if (args[0].contains("l") && args[0].startsWith("-")) {
-      System.out.println("LETTER");
-    } else if (args[0].contains("a") && args[0].startsWith("-")) {
-      System.out.println("APPLE");
-    } else if (args[0].contains("r") && args[0].startsWith("-")) {
-      System.out.println("ROBOT");
-    } else if (args[0].contains("c") && args[0].startsWith("-")) {
-      System.out.println("CENTER");
-    }
+    ToDoList todoList1 = new ToDoList();
+    todoList1.listTasks(args);
   }
 }
--- ToDoApp/src/ToDoList.java
+++ ToDoApp/src/ToDoList.java
@@ -1,3 +1,55 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class ToDoList {
 
+  public void listTasks (String[] args) {
+    if (args.length == 0) {
+      printUsageInformation();
+    } else if (args[0].contains("l") && args[0].startsWith("-")) {
+      readListLines();
+    } else if (args[0].contains("a") && args[0].startsWith("-")) {
+      System.out.println("APPLE");
+    } else if (args[0].contains("r") && args[0].startsWith("-")) {
+      System.out.println("ROBOT");
+    } else if (args[0].contains("c") && args[0].startsWith("-")) {
+      System.out.println("CENTER");
+    }
+  }
+
+  public void printUsageInformation() {
+    System.out.println(
+        "\n" +
+            "*****************************\n" +
+            "* Java Todo application     *\n" +
+            "* =====================     *\n" +
+            "*                           *\n" +
+            "* Command line arguments:   *\n" +
+            "*  -l   Lists all the tasks *\n" +
+            "*  -a   Adds a new task     *\n" +
+            "*  -r   Removes a task      *\n" +
+            "*  -c   Completes a task    *\n" +
+            "*****************************\n" +
+            "\n"
+    );
+  }
+
+  public void readListLines() {
+    try {
+      Path listPath = Paths.get("list.txt");
+      List<String> lines = Files.readAllLines(listPath);
+
+      int lineNr = 1;
+      if (lines.get(0) != null) {
+        for (int i = 0; i < lines.size(); i++) {
+          System.out.println(lineNr + " - " + lines.get(i));
+          lineNr++;
+        }
+      }
+    } catch (Exception e) {
+      System.out.println("No todos for today! :)");
+    }
+  }
 }
