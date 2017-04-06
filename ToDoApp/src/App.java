f5bcefca2d4589dd6e9bbbd07865127186c1acda
Daniel Szalay <sza.dan.uj@gmail.com>
2017-04-06 11:35:28 +0200

    add gitignore, start ToDoApp

---
+++ ToDoApp/src/App.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class App {
+
+  public static void main(String[] args) {
+    System.out.println(
+        "\n" +
+        "Java Todo application\n" +
+        "=====================\n" +
+        "\n"  +
+        "Command line arguments:\n" +
+        " -l   Lists all the tasks\n" +
+        " -a   Adds a new task\n" +
+        " -r   Removes a task\n" +
+        " -c   Completes a task\n"
+    );
+  }
+}
