fc7a9907a4378931c73d0098911e5e850b2ea582
Daniel Szalay <sza.dan.uj@gmail.com>
2017-04-07 12:07:11 +0200

    add checkTask function

--- ToDoApp/src/ToDoList.java
+++ ToDoApp/src/ToDoList.java
@@ -2,11 +2,7 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
-
 
 public class ToDoList {
 
@@ -19,9 +15,8 @@ public class ToDoList {
       addNewTask(args);
     } else if (args[0].contains("r") && args[0].startsWith("-")) {
       removeTask(args);
-      readListLines();
     } else if (args[0].contains("c") && args[0].startsWith("-")) {
-      System.out.println("complete task");
+      checkTask(args);
     } else if ((!args[0].startsWith("-")) || (args[0].length() > 2)) {
       unsupportedArgument();
     }
@@ -68,7 +63,7 @@ public class ToDoList {
       lines.add(args[1]);
       Files.write(listPath, lines, Charset.defaultCharset());
     } catch (Exception e) {
-      System.out.println("Whooopsie, and error has occurred!");
+      System.out.println("Unable to add: no task provided!");
     }
   }
 
@@ -76,14 +71,64 @@ public class ToDoList {
     try {
       Path listPath = Paths.get("list.txt");
       List<String> lines = Files.readAllLines(listPath);
+      int argLineNumber = Integer.parseInt(args[1]);
 
-      if (lines.size() >= 2) {
-        int argLineNumber = Integer.parseInt(args[1]);
+      if (args.length < 2) {
+        System.out.println("Unable to remove: no index provided");
+      } else if (lines.size() < argLineNumber) {
+        System.out.println("Unable to remove: index out of bound");
+      } else if (lines.size() >= 2 && lines.size() > argLineNumber) {
         lines.remove(argLineNumber - 1);
       }
       Files.write(listPath, lines, Charset.defaultCharset());
+    } catch (Exception notAnInt) {
+      if (args.length > 1) {
+        System.out.println("Unable to remove: index not a number");
+      }
+    }
+  }
+
+  public void checkTask (String[] args) {
+    try {
+      Path listPath = Paths.get("list.txt");
+      List<String> lines = Files.readAllLines(listPath);
+
+      int argLineNumber = Integer.parseInt(args[1])-1;
+      int lineNr = 0;
+
+      if (argLineNumber > lines.size()) {
+        System.out.println("Unable to check: index is out of bound");
+      }
+
+      for (int i=0; i < lines.size(); i++) {
+        if (!lines.get(i).startsWith("[ ]") && !lines.get(i).startsWith("[X]") ) {
+          String buffer1 = lines.get(i);
+          lines.remove(i);
+          lines.add(i, "[ ]" + buffer1);
+        }
+      }
+
+      for (int i=0; i < lines.size(); i++) {
+        if (lines.get(i).startsWith("[X]")) {
+          String buffer = lines.get(i);
+          lines.remove(i);
+          lines.add(i, buffer);
+          lineNr++;
+        } else if (lines.get(i).startsWith("[ ]") && (lineNr != argLineNumber)) {
+          String buffer = lines.get(i);
+          lines.remove(i);
+          lines.add(i, buffer);
+          lineNr++;
+        } else if (lines.get(i).startsWith("[ ]") && (lineNr == argLineNumber)) {
+          String buffer = lines.get(i).substring(3);
+          lines.remove(i);
+          lines.add(i, "[X]" + buffer);
+          lineNr++;
+        }
+      }
+      Files.write(listPath, lines, Charset.defaultCharset());
     } catch (Exception e) {
-      System.out.println("Whooopsie, and error has occurred!");
+      System.out.println("Unable to check: no index provided");
     }
   }
 
